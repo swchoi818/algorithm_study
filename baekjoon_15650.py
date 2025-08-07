7d72dd0688cd88eaf4c409fd00cf7f56c534b669
swChoi <chsw030818@naver.com>
2025-08-07 11:29:11 +0900

    백준 백트래킹 입문 문제

---
+++ baekjoon_15650.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+
+result = []
+
+def search_num(next_choice, selected_num = []):
+    global result
+    if len(selected_num) == M:
+        result.append(selected_num[:])
+    
+    for i in next_choice:
+        selected_num.append(i)
+        nxtchc = next_choice[next_choice.index(i)+1:]
+        search_num(nxtchc, selected_num)
+        selected_num.pop()
+
+search_num(list(range(1,N+1)))
+for i in result:
+    print(*i, sep = ' ')
