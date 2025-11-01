c73aede80239d64735090bc3a6910b169f8b2d53
swChoi <chsw030818@naver.com>
2025-11-01 19:50:21 +0900

    백준 16236 아기 상어 (bfs)

--- baekjoon_16236.py
+++ baekjoon_16236.py
@@ -1,5 +1,6 @@
 import sys
 from collections import deque
+import heapq
 
 DIR = [(-1, 0), (0, -1), (0, 1), (1, 0)]
 
@@ -10,6 +11,7 @@ mtr = [list(map(int, input().split())) for _ in range(N)]
 
 shark_size = 2
 time = 0
+result = 0
 dq = deque()
 visited = [[False] * N for _ in range(N)]
 
@@ -17,11 +19,29 @@ for i in range(N):
     for j in range(N):
         if mtr[i][j] == 9:
             visited[i][j] = True
+            mtr[i][j] = 0
             dq.append((i, j, 0, 0))
             break
 
+nearest_fish = []
+
 while dq:
     y, x, e, t = dq.popleft()
+    if t == time and time > 0:
+        e += 1
+        y, x = heapq.heappop(nearest_fish)
+        mtr[y][x] = 0
+        if e == shark_size:
+            shark_size += 1
+            e = 0
+        visited = [[False] * N for _ in range(N)]
+        visited[y][x] = True
+        dq.clear()
+        nearest_fish.clear()
+        dq.append((y, x, e, time))
+        result = time
+        time = 0
+        continue
     for dy, dx in DIR:
         ny = y + dy
         nx = x + dx
@@ -32,16 +52,9 @@ while dq:
                 visited[ny][nx] = True
                 dq.append((ny, nx, e, t + 1))
                 continue
-            e += 1
-            mtr[ny][nx] = 0
-            if e == shark_size:
-                shark_size += 1
-                e = 0
-            visited = [[False] * N for _ in range(N)]
+            heapq.heappush(nearest_fish, (ny, nx))
             time = t + 1
-            dq.clear()
             visited[ny][nx] = True
-            dq.append((ny, nx, e, time))
-            break
+            dq.append((ny, nx, e, t + 1))
 
-print(time)
+print(result)
